4d760d34108553954dae7125be2623dfe46eca93
Lorenzo Fontoura <lorenzo.fontoura@dltx.io>
2021-06-23 14:41:04 +1000

    wait for txs to confirm in mintWithEthAndKeeperPool.spec.ts

--- tests/suites/mintWithEthAndKeeperPool.spec.ts
+++ tests/suites/mintWithEthAndKeeperPool.spec.ts
@@ -22,21 +22,21 @@ describe("Vault: mintWithEth, fxKeeperPool", function () {
     expect(keeperPool).not.toBeNull();
   });
   it("Should mint with ether as collateral", async () => {
-    await vault.mintWithEth(
+    await (await vault.mintWithEth(
       ethers.utils.parseEther("0.000001"), // .00001 cent of fxAUD
       ethers.utils.parseEther("0.00000027"), // approximately  0.001 AUD in Ether
       false
-    );
+    )).wait(1);
     expect(vault.debt.gt(0)).toBeTruthy();
   });
   it("Should allow to stake fxTokens in keeper pool", async() => {
-    await keeperPool.stake(ethers.BigNumber.from("10")); // 10 wei.
+    await (await keeperPool.stake(ethers.BigNumber.from("10"))).wait(1); // 10 wei.
   });
   it("Should return balanceOfStake from keeper pool instance", async() => {
     const balanceOfStake = await keeperPool.balanceOfStake(vault.account);
     expect(balanceOfStake.gt(0)).toBeTruthy();
   });
   it("Should allow to unstake fxTokens from keeper pool", async() => {
-    await keeperPool.unstake(ethers.BigNumber.from("10")); // 10 wei.
+    await (await keeperPool.unstake(ethers.BigNumber.from("10"))).wait(1); // 10 wei.
   });
 });
