6db5648e441d8d9dae5e88386136a81867319759
Lorenzo Fontoura <lorenzo.fontoura@dltx.io>
2021-06-23 14:13:30 +1000

    add arrays for token enums

--- src/types/ProtocolTokens.ts
+++ src/types/ProtocolTokens.ts
@@ -9,3 +9,25 @@ export enum CollateralTokens {
   WBTC = "WBTC",
   DAI = "DAI"
 }
+
+export const fxTokensArray = (() => {
+  // Enums in typescript turn into a dictionary with index and value keys,
+  // therefore the number of keys is duplicated.
+  const keys = Object.keys(fxTokens);
+  const values = [];
+  for (let i = 0; i < keys.length/2; i++) {
+    values.push(keys[i]);
+  }
+  return values;
+})();
+
+export const collateralTokensArray = (() => {
+  // Enums in typescript turn into a dictionary with index and value keys,
+  // therefore the number of keys is duplicated.
+  const keys = Object.keys(CollateralTokens);
+  const values = [];
+  for (let i = 0; i < keys.length/2; i++) {
+    values.push(keys[i]);
+  }
+  return values;
+})();
