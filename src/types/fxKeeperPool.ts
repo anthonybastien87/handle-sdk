3bdd0444542759cff9a931b818fc122ed0633ebc
Lorenzo Fontoura <lorenzo.fontoura@dltx.io>
2021-06-23 14:39:23 +1000

    fix return types for getters

--- src/types/fxKeeperPool.ts
+++ src/types/fxKeeperPool.ts
@@ -10,6 +10,7 @@ import {CollateralToken} from "./CollateralToken";
 export class fxKeeperPool {
   public token: string;
   private contract: ethers.Contract;
+  public gasLimit: ethers.BigNumber;
 
   /**
    * @param token The pool fxToken address.
@@ -18,41 +19,50 @@ export class fxKeeperPool {
   public constructor(token: string, contract: ethers.Contract) {
     this.token = token;
     this.contract = contract;
+    this.gasLimit = ethers.BigNumber.from("750000");
   }
   
   public async stake(amount: ethers.BigNumber) {
     if (amount.lt(0))
       throw new Error("Amount must be greater than 0");
-    return await this.contract.stake(amount, this.token);
+    return await this.contract.stake(amount, this.token, {
+      gasLimit: this.gasLimit
+    });
   }
   
   public async unstake(amount: ethers.BigNumber) {
     if (amount.lt(0))
       throw new Error("Amount must be greater than 0");
-    return await this.contract.unstake(amount, this.token);
+    return await this.contract.unstake(amount, this.token, {
+      gasLimit: this.gasLimit
+    });
   }
   
   public async withdrawCollateralReward() {
-    return await this.contract.withdrawCollateralReward(this.token);
+    return await this.contract.withdrawCollateralReward(this.token, {
+      gasLimit: this.gasLimit
+    });
   }
   
-  public async balanceOfStake(account: string) {
+  public async balanceOfStake(account: string): Promise<ethers.BigNumber> {
     return await this.contract.balanceOfStake(account, this.token);
   }
   
-  public async balanceOfRewards(account: string) {
+  public async balanceOfRewards(account: string): Promise<ethers.BigNumber> {
     return await this.contract.balanceOfRewards(account, this.token);
   }
   
-  public async shareOf(account: string) {
+  public async shareOf(account: string): Promise<ethers.BigNumber> {
     return await this.contract.shareOf(account, this.token);
   }
   
   public async liquidate(account: string) {
-    return await this.contract.liquidate(account, this.token);
+    return await this.contract.liquidate(account, this.token, {
+      gasLimit: this.gasLimit
+    });
   }
   
-  public async getPoolCollateralBalance(collateral: CollateralToken) {
+  public async getPoolCollateralBalance(collateral: CollateralToken): Promise<ethers.BigNumber> {
     return await this.contract.getPoolCollateralBalance(
       this.token,
       collateral.address
