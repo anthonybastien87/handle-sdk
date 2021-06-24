34e18fd16d7b17266febc6acec2526f057589ffe
Lorenzo Fontoura <lorenzo.fontoura@dltx.io>
2021-06-24 12:25:31 +1000

    HP-476 (#5)
    * add fxKeeperPool class
    
    * update fxKeeperPool.ts
    
    * add arrays for token enums
    
    * initialise keeper pool dictionary
    
    * check for env variables before running unit tests
    
    * rename load.ts to load.spec.ts
    
    * remove unused line in fxKeeperPool.ts
    
    * move unit tests to suites folder
    
    * export fxKeeperPool
    
    * fix return types for getters
    
    * add fxKeeperPool tests in mintWithEthAndKeeperPool.spec.ts
    
    * run jest sequentially (--runInBand)
    
    * wait for txs to confirm in mintWithEthAndKeeperPool.spec.ts
    
    * fix initialisation of fxKeeperPool
    
    * ensure erc20 allowance when staking into keeper pool
    
    * fix erc20.allowance call
    
    * remove runInBand flag from jest
    
    * patch sdk version

---
+++ src/types/fxKeeperPool.ts
@@ -0,0 +1,94 @@
+import { ethers } from "ethers";
+import {CollateralToken} from "./CollateralToken";
+import {SDK} from "./SDK";
+import {fxTokens} from "./ProtocolTokens";
+
+/**
+ * Class to interact with a fxKeeperPool for a specific fxToken.
+ * The fxKeeperPool is a single contract, but each function requires an
+ * input fxToken to perform the action for. This class fills the fxToken
+ * parameters with the instance token.
+ */
+export class fxKeeperPool {
+  private contract: ethers.Contract;
+  /** fxToken contract */
+  private erc20: ethers.Contract;
+  public token: string;
+  public gasLimit: ethers.BigNumber;
+
+  /**
+   * @param sdk The SDK instance.
+   * @param token The pool fxToken address.
+   * @param contract The keeper pool contract instance.
+   */
+  public constructor(sdk: SDK, token: fxTokens, contract: ethers.Contract) {
+    this.erc20 = sdk.contracts[token];
+    this.token = this.erc20.address;
+    this.contract = contract;
+    this.gasLimit = ethers.BigNumber.from("750000");
+  }
+
+  /**
+   * Sets allowance to pool fxToken if needed.
+   * Requires erc20 contract to be connected to signer.
+   */
+  private async ensureAllowance(amount: ethers.BigNumber) {
+    const allowance: ethers.BigNumber = await this.erc20
+      .allowance(await this.erc20.signer.getAddress(), this.contract.address);
+    if (allowance.gte(amount))
+      return;
+    await (await this.erc20.approve(this.contract.address, amount)).wait(1);
+  }
+  
+  public async stake(amount: ethers.BigNumber) {
+    if (amount.lt(0))
+      throw new Error("Amount must be greater than 0");
+    await this.ensureAllowance(amount);
+    return await this.contract.stake(amount, this.token, {
+      gasLimit: this.gasLimit
+    });
+  }
+  
+  public async unstake(amount: ethers.BigNumber) {
+    if (amount.lt(0))
+      throw new Error("Amount must be greater than 0");
+    return await this.contract.unstake(amount, this.token, {
+      gasLimit: this.gasLimit
+    });
+  }
+  
+  public async withdrawCollateralReward() {
+    return await this.contract.withdrawCollateralReward(this.token, {
+      gasLimit: this.gasLimit
+    });
+  }
+  
+  public async balanceOfStake(account: string): Promise<ethers.BigNumber> {
+    return await this.contract.balanceOfStake(account, this.token);
+  }
+  
+  public async balanceOfRewards(account: string): Promise<ethers.BigNumber> {
+    return await this.contract.balanceOfRewards(account, this.token);
+  }
+  
+  public async shareOf(account: string): Promise<ethers.BigNumber> {
+    return await this.contract.shareOf(account, this.token);
+  }
+  
+  public async liquidate(account: string) {
+    return await this.contract.liquidate(account, this.token, {
+      gasLimit: this.gasLimit
+    });
+  }
+  
+  public async getPoolCollateralBalance(collateral: CollateralToken): Promise<ethers.BigNumber> {
+    return await this.contract.getPoolCollateralBalance(
+      this.token,
+      collateral.address
+    );
+  }
+  
+  public async getPoolTotalDeposit() {
+    return await this.contract.getPoolTotalDeposit(this.token);
+  }
+}
--- tests/suites/mintWithEthAndKeeperPool.spec.ts
+++ tests/suites/mintWithEthAndKeeperPool.spec.ts
@@ -4,11 +4,13 @@ import { Vault } from "../../src/types/Vault";
 import { fxTokens } from "../../src/types/ProtocolTokens";
 import { ethers } from "ethers";
 import { getSDK } from "../setupTests";
+import {fxKeeperPool} from "../../src";
 
 let sdk: SDK;
 let vault: Vault;
+let keeperPool: fxKeeperPool;
 
-describe("Vault: mintWithEth", function () {
+describe("Vault: mintWithEth, fxKeeperPool", function () {
   beforeAll(() => {
     sdk = getSDK();
   });
@@ -16,13 +18,25 @@ describe("Vault: mintWithEth", function () {
     // @ts-ignore
     const account = await sdk.signer.getAddress();
     vault = await Vault.from(account, fxTokens.fxAUD, sdk);
+    keeperPool = sdk.keeperPools[fxTokens.fxAUD];
+    expect(keeperPool).not.toBeNull();
   });
   it("Should mint with ether as collateral", async () => {
-    await vault.mintWithEth(
+    await (await vault.mintWithEth(
       ethers.utils.parseEther("0.000001"), // .00001 cent of fxAUD
       ethers.utils.parseEther("0.00000027"), // approximately  0.001 AUD in Ether
       false
-    );
+    )).wait(1);
     expect(vault.debt.gt(0)).toBeTruthy();
+  });
+  it("Should allow to stake fxTokens in keeper pool", async() => {
+    await (await keeperPool.stake(ethers.BigNumber.from("10"))).wait(1); // 10 wei.
+  });
+  it("Should return balanceOfStake from keeper pool instance", async() => {
+    const balanceOfStake = await keeperPool.balanceOfStake(vault.account);
+    expect(balanceOfStake.gt(0)).toBeTruthy();
+  });
+  it("Should allow to unstake fxTokens from keeper pool", async() => {
+    await (await keeperPool.unstake(ethers.BigNumber.from("10"))).wait(1); // 10 wei.
   });
 });
