97d852c19e0d3782b14b1e7fa65bbcab6746376f
David Nabb <David@N4Design.com.au>
2021-10-05 10:36:58 +1100

    Fix test

--- tests/suites/convert.spec.ts
+++ tests/suites/convert.spec.ts
@@ -2,22 +2,24 @@ import { describe, it } from "@jest/globals";
 import { Convert } from "../../src";
 import { ethers } from "ethers";
 
-let convert: Convert;
+let convertList: Convert;
+let convertSwap: Convert;
 
 describe("Convert", function () {
   beforeAll(async () => {
-    convert = new Convert("homestead");
+    convertList = new Convert("homestead");
+    convertSwap = new Convert("kovan");
   });
 
   it("Should return a list of tokens", async () => {
-    const tokens = await convert.getTokens();
+    const tokens = await convertList.getTokens();
     expect(tokens.length).toBeGreaterThan(0);
   });
 
   it("Should return different tokens when the network changes", async () => {
     const SECOND_NETWORK = "polygon";
 
-    const networkOneTokens = await convert.getTokens();
+    const networkOneTokens = await convertList.getTokens();
 
     if (process.env.NETWORK === SECOND_NETWORK) {
       throw new Error("ENV network is the same as SECOND_NETWORK");
@@ -31,7 +33,7 @@ describe("Convert", function () {
   });
 
   it("Should fetch price data for a trade", async () => {
-    const priceResult = await convert.getQuote(
+    const priceResult = await convertSwap.getQuote(
       "ETH",
       "0x4f96fe3b7a6cf9725f59d353f723c1bdb64ca6aa",
       ethers.utils.parseEther("1"),
